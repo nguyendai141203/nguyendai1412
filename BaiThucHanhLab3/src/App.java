6b240a7d2cf55bf206ca47382710df04283f82f8
nguyendai141203 <nguyendai211203@gmail.com>
2023-04-12 22:49:10 +0700

    BaiThucHanhLab3

---
+++ BaiThucHanhLab3/src/App.java
@@ -0,0 +1,18 @@
+
+public class App {
+
+	public static void main(String[] args) {
+//		NhanVien a = new NhanVien();
+//		a.NhapThongTin();
+//		a.InThongTin();
+		
+//		HinhTron a = new HinhTron();
+//		a.NhapBanKinh();
+//		a.InThongTin();
+		
+		HinhTruTron a = new HinhTruTron();
+		a.NhapThongTin();
+		a.InThongTin();
+	}
+
+}
